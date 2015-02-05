865ddbc2e71b3244f08dc3ecf38e5833b82b119a
Guillaume <guillaume@couleur-citron.com>
2015-02-05 12:26:02 +0100

    Ajout $oUser en global

--- autoClass/maj.fkey.php
+++ autoClass/maj.fkey.php
@@ -11,6 +11,7 @@ global $default_cms_site_name;
 global $isRecursive;
 global $aOps;
 global $sql;
+global $oUser;
 
 if (empty($aNodeToSort)) {
 	 
@@ -773,6 +774,7 @@ function displayOptionSelect($sql, $res, $stack, $oTemp, $aCurrentNode, $eKeyVal
 	global $isRecursive;
 	global $aOps;
 	global $db;
+	global $oUser;
 
 	$aListeChamps = $oTemp->getListeChamps();
 	$foreignPrefixe = $stack[0]["attrs"]["PREFIX"];
@@ -869,8 +871,6 @@ function displayOptionSelect($sql, $res, $stack, $oTemp, $aCurrentNode, $eKeyVal
 			}
 
 		} else {
-
-			
 			if ((($aCurrentNode["attrs"]["NAME"] != 'rank')&&($aCurrentNode["attrs"]["NAME"] != 'bo_groupes'))||
 				(($aCurrentNode["attrs"]["NAME"] == 'rank')&&($oUser->get_rank()<=$tempId)&&($oUser->get_rank()!=-1)) ||
 				(($aCurrentNode["attrs"]["NAME"] == 'bo_groupes')&&($oUser->get_bo_groupes()==$tempId)) ||
