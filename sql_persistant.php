a5af48544652e23cbac3cc817fdcbd5e80b61a06
Pierre Priot <pierre@couleur-citron.com>
2015-06-18 10:59:42 +0200

    merge whitespace

--- sql_persistant.php
+++ sql_persistant.php
@@ -204,7 +204,6 @@ function dbGetObjectsFromFieldValue($sObjet, $aGetterWhere, $aValeurChamp, $sGet
 function dbGetObjectsFromFieldValue2($sObjet, $aGetterWhere, $aValeurChamp, $aGetterOrderBy, $aGetterSensOrderBy) {
 
 	$sRequete = dbGetSQLFromFieldValue3($sObjet, $aGetterWhere, null, $aValeurChamp, $aGetterOrderBy, $aGetterSensOrderBy);
-	
 	//print("<br/>".$sRequete);
 
 	return dbGetObjectsFromRequete($sObjet, $sRequete);
