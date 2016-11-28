c022529e2b17d3df21f17f58222e03e8c2ee7a28
Pierre Priot <pierre@couleur-citron.com>
2016-11-28 17:27:34 +0100

    compat php7

--- class/cms_jquery_version.class.php
+++ class/cms_jquery_version.class.php
@@ -2,12 +2,6 @@
 //pre_dump("jquery_version");
 /* [Begin patch] */
 
-//if(in_array('cms_site', $_SESSION['patches'])){
-//    $key = array_search('cms_site', $_SESSION['patches']);
-//    
-//    unset($_SESSION['patches'][$key]);
-//}
-
 //pre_dump(ispatched('cms_jquery_version'));
 if (!ispatched('cms_jquery_version')){
     //pre_dump("patch");
@@ -26,8 +20,8 @@ if (!ispatched('cms_jquery_version')){
         //pre_dump("ok table");
         
         $sSQl = "SELECT * FROM cms_jquery_version";
-        $result = mysql_query($sSQl);
-        $number = mysql_num_rows($result);
+        $result = mysqli_query($sSQl);
+        $number = mysqli_num_rows($result);
         //pre_dump($number);
         if($number == "0"){
             //pre_dump("no data");
